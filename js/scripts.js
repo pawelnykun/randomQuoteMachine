ef51e9aeb5631167d10c9a2487e690c6565f49c3
pawelnykun <pawel.nykun@gmail.com>
2017-05-11 14:13:05 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,28 @@
 $(function() {
 	var tweetLink = "https://twitter.com/intent/tweet?text=";
-	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-	//"http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+	var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
 	function getQuote() {
 		$.getJSON(quoteUrl, createTweet);
 	}
 
 	function createTweet(input) {
-		var data = input[0];
-		var quoteText = $(data.content).text().trim();
-		var quoteAuthor = data.title;
-		
-		if (!quoteAuthor.length) {
-			quoteAuthor = "Unknown author";
-		}	
-		
-		var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
+		var tweetText = "Quote of the day - " + input.quoteText + "    Author: " + input.quoteAuthor;
+		if (!input.quoteAuthor.length) {
+			input.quoteAuthor = "Unknown author";
+		}
 		if (tweetText.length > 140) {
 			getQuote();
 		} else {
 			var tweet = tweetLink + encodeURIComponent(tweetText);
-			$('.quote').text(quoteText);
-			$('.author').text("Author: " + quoteAuthor);
+			$('.quote').text('„' + input.quoteText + '”');
+			$('.author').text("Author: " + input.quoteAuthor);
 			$('.tweet').attr('href', tweet);
 		}
 	}
 
-	$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
 		getQuote();
-		$('.trigger').click(function() {
-			getQuote();
-		})
 	});
 });
